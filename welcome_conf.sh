f520f8fbea92316eb8863f131d2d644e40fa03e0
Dragomir Ivanov <49022669+murhum@users.noreply.github.com>
2020-01-11 22:42:52 +0200

    Update welcome_conf.sh

--- welcome_conf.sh
+++ welcome_conf.sh
@@ -6,7 +6,7 @@ yum clean all
 
 yum install epel-release -y
 
-yum install python-pip -y 
+yum install python-pip -y
 
 yum install python-devel -y
 
@@ -18,19 +18,36 @@ wait 10
 
 yum update -y
 
-yum install epel-release -y 
+yum install epel-release -y
 
-yum install ansible -y 
+yum install ansible -y
 
 ansible --version
 
-wait 10 
+wait 10
 
 yum update -y
 
 yum clean all
 
-yum install git -y 
+yum install git -y
 
 yum update -y
 
+$ yum install -y yum-utils device-mapper-persistent-data lvm2 -y
+
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install docker-ce -y
+
+usermod -aG docker $(whoami)
+
+systemctl enable docker.service
+
+systemctl start docker.service
+
+yum install epel-release -y
+
+pip install docker-compose
+
+yum upgrade python* -y
